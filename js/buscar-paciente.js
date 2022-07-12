7b0a7a2d3195713f87cefee37671db96f17944e7
Carlos Eduardo Gomes <85941223+dudugomes01@users.noreply.github.com>
2022-07-11 21:58:14 -0300

    Ajustando API

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -4,12 +4,14 @@ botaoAdicionar.addEventListener("click", function(){
     
     var xhr = new XMLHttpRequest()
 
-    xhr.open("GET", "http://api-paci11111entes.herokuapp.com/pacientes")
+    xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes")
 
     xhr.addEventListener("load", function(){
         var erroAjax = document.querySelector("#erro-ajax")
+        
 
         if(xhr.status == 200){
+            erroAjax.classList.add("invisivel")
             var resposta = xhr.responseText
             var pacientes = JSON.parse(resposta)
 
@@ -17,7 +19,9 @@ botaoAdicionar.addEventListener("click", function(){
                 adicionaPacienteNaTabela(paciente)
             })
         }else{
-            console.log(xhr.status)
+            console.log("xhr.status");
+            console.log(xhr.status);
+            console.log(xhr.responseText);
             erroAjax.classList.remove("invisivel");
         }
         
